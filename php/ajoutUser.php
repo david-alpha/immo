aacd17057fbdd120cbed66adb5132f26c8eb672a
david@alpha.fr <david@alpha.fr>
2021-05-27 09:46:09 +0000

    insertion de l'ajout user

--- php/ajoutUser.php
+++ php/ajoutUser.php
@@ -1,5 +1,5 @@
 <?php
-include 'template/connect.php';
+include '../template/connect.php';
 
      $civilite = $_POST['civilite']; 
      $nom = $_POST['nom']; 
@@ -8,8 +8,18 @@ include 'template/connect.php';
      $role = $_POST['role'];
      $email = $_POST['email'];
    echo "$nom, $prenom, $civilite, $password, $role, $email";
-
-
-
+//Insertion en base de données -- Debut
+	$data = [
+    'civilite' => $civilite,
+    'nom' => $nom,
+    'prenom' => $prenom,
+    'email' => $email,
+    'password' => $password,
+    'role' => $role,
+	];
+	$sql = "INSERT INTO user (civilite, nom, prenom, email, password, role) VALUES (:civilite, :nom, :prenom, :email, :password, :role)";
+	$stmt= $dbh->prepare($sql);
+	$stmt->execute($data);
+//Insertion en base de données -- Fin
 
 ?>
